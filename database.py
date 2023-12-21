479b2841a6034f5c499a6b2fd37a4dc3d839818b
A. Cid <lordix.acid@gmail.com>
2023-12-21 20:28:17 -0300

    Archivos

---
+++ database.py
@@ -0,0 +1,157 @@
+import sqlite3
+from datetime import datetime
+
+
+class Database:
+    def __init__(self):
+        self.con = sqlite3.connect('data.db')
+        self.cursor = self.con.cursor()
+        self.create_user_table()
+        self.create_books_table()
+
+        if not self.search_books("Don Quijote"):
+            self.inserts_books_by_default()
+
+    def create_user_table(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS usuario(id integer PRIMARY KEY AUTOINCREMENT, usuario varchar(60) NOT NULL, password varchar(60) NOT NULL, nombre varchar(60) NOT NULL, apellido varchar(60) NOT NULL, logged_in integer NOT NULL DEFAULT 0)")
+        self.con.commit()
+
+    def create_user(self, usuario, password, nombre, apellido):
+        self.cursor.execute(
+            "INSERT INTO usuario(usuario, password, nombre, apellido, logged_in) VALUES(?, ?, ?, ?, 0)", (usuario, password, nombre, apellido))
+        self.con.commit()
+
+    def get_user(self, username):
+        user = self.cursor.execute(
+            "SELECT * FROM usuario WHERE usuario=?", (username,)).fetchone()
+
+        if user:
+            return {'status': True, 'data': user}
+        else:
+            return {'status': False}
+
+    def log_out_user(self, username):
+        self.cursor.execute(
+            "UPDATE usuario SET logged_in = 0 WHERE usuario = ?", (username,))
+        self.con.commit()
+
+    def create_books_table(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS libros(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, autor TEXT NOT NULL, fecha TEXT NOT NULL, precio REAL NOT NULL, categoria TEXT NOT NULL, descripcion TEXT NOT NULL, idioma TEXT NOT NULL)")
+        self.con.commit()
+
+    def inserts_books_by_default(self):
+        books = [
+            {
+                "titulo": "Don Quijote",
+                "autor": "Miguel de Cervantes",
+                "fecha": datetime(1605, 1, 1),
+                "precio": 15000,
+                "categoria": "Novela",
+                "descripcion": "Don Quijote de la Mancha es una novela que satiriza las novelas de caballería y la sociedad de la época.",
+                "idioma": "Español"
+            },
+            {
+                "titulo": "Cien años de soledad",
+                "autor": "Gabriel García Márquez",
+                "fecha": datetime(1967, 1, 1),
+                "precio": 18000,
+                "categoria": "Realismo mágico",
+                "descripcion": "Cien años de soledad es una novela que explora la historia de la familia Buendía en el pueblo ficticio de Macondo.",
+                "idioma": "Español"
+            },
+            {
+                "titulo": "El señor de los anillos",
+                "autor": "J.R.R. Tolkien",
+                "fecha": datetime(1954, 1, 1),
+                "precio": 22000,
+                "categoria": "Fantasía",
+                "descripcion": "El señor de los anillos es una trilogía épica que sigue la búsqueda de un anillo para destruir el mal en la Tierra Media.",
+                "idioma": "Inglés"
+            },
+            {
+                "titulo": "El código Da Vinci",
+                "autor": "Dan Brown",
+                "fecha": datetime(2003, 1, 1),
+                "precio": 19000,
+                "categoria": "Thriller",
+                "descripcion": "El código Da Vinci es un thriller que sigue al profesor Robert Langdon en una búsqueda de secretos ocultos en obras de arte.",
+                "idioma": "Inglés"
+            },
+            {
+                "titulo": "Harry Potter y la piedra filosofal",
+                "autor": "J.K. Rowling",
+                "fecha": datetime(1997, 1, 1),
+                "precio": 25000,
+                "categoria": "Fantasía",
+                "descripcion": "La primera entrega de la serie Harry Potter sigue las aventuras del joven mago Harry en Hogwarts.",
+                "idioma": "Inglés"
+            },
+            {
+                "titulo": "El retrato de Dorian Gray",
+                "autor": "Oscar Wilde",
+                "fecha": datetime(1890, 1, 1),
+                "precio": 17000,
+                "categoria": "Novela gótica",
+                "descripcion": "El retrato de Dorian Gray es una novela que explora la moralidad y la decadencia a través del retrato envejeciente de un hombre.",
+                "idioma": "Inglés"
+            },
+            {
+                "titulo": "El perfume",
+                "autor": "Patrick Süskind",
+                "fecha": datetime(1985, 1, 1),
+                "precio": 20000,
+                "categoria": "Novela histórica",
+                "descripcion": "El perfume sigue la vida de Jean-Baptiste Grenouille, un asesino obsesionado con el sentido del olfato.",
+                "idioma": "Alemán"
+            },
+            {
+                "titulo": "El alquimista",
+                "autor": "Paulo Coelho",
+                "fecha": datetime(1988, 1, 1),
+                "precio": 16000,
+                "categoria": "Novela filosófica",
+                "descripcion": "El alquimista narra el viaje de Santiago en busca de su tesoro personal y la realización de sus sueños.",
+                "idioma": "Portugués"
+            }
+        ]
+
+        for book in books:
+            self.cursor.execute("INSERT INTO libros (titulo, autor, fecha, precio, categoria, descripcion, idioma) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                                (book["titulo"], book["autor"], book["fecha"], book["precio"], book["categoria"], book["descripcion"], book["idioma"]))
+
+            self.con.commit()
+
+    def insert_book(self, titulo, autor, fecha, precio, categoria, descripcion, idioma):
+        self.cursor.execute(
+            "INSERT INTO libros(titulo, autor, fecha, precio, categoria, descripcion, idioma) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (titulo, autor, fecha, precio, categoria, descripcion, idioma)
+        )
+        self.con.commit()
+
+    def delete_book(self, book_id):
+        self.cursor.execute("DELETE FROM libros WHERE id=?", (book_id,))
+        self.con.commit()
+
+    def update_book(self, book_id, titulo, autor, fecha, precio, categoria, descripcion, idioma):
+        self.cursor.execute(
+            "UPDATE libros SET titulo=?, autor=?, fecha=?, precio=?, categoria=?, descripcion=?, idioma=? WHERE id=?",
+            (titulo, autor, fecha, precio, categoria,
+             descripcion, idioma, book_id)
+        )
+        self.con.commit()
+
+    def search_books(self, search):
+        books = self.cursor.execute(
+            "SELECT * FROM libros WHERE titulo LIKE ? OR autor LIKE ?",
+            (f"%{search}%", f"%{search}%")
+        ).fetchone()
+        return books
+
+    def get_all_books(self):
+        books = self.cursor.execute("SELECT * FROM libros").fetchall()
+        return books
+
+    def close_db_connection(self):
+        self.con.close()
---
+++ main.py
@@ -0,0 +1,306 @@
+
+from datetime import datetime
+import sys
+import os
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.card import MDCard
+from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+from kivy.utils import platform
+from kivy.core.window import Window
+from database import Database
+
+if hasattr(sys, '_MEIPASS'):
+    os.environ['KIVY_NO_CONSOLELOG'] = '1'
+
+database = Database()
+
+
+def validate_empty(*args):
+    errors = False
+
+    for campo in args:
+        if campo.text == "":
+            campo.error = True
+            errors = True
+
+    return errors
+
+
+class LoginScreen(MDScreen):
+    def login(self):
+        username = self.ids.user
+        password = self.ids.password
+
+        empty = validate_empty(username, password)
+
+        if empty:
+            self.ids.messages.text = "Todos los campos son requeridos."
+            return
+
+        res = database.get_user(username.text)
+
+        if (res['status'] == False):
+            app_instance = MDApp.get_running_app()
+            app_instance.show_dialog()
+        else:
+            if (res['data'][2] != password.text):
+                self.ids.messages.text = "Contraseña incorrecta."
+                password.error = True
+                return
+
+            self.manager.transition.direction = 'left'
+            self.manager.current = 'app_screen'
+
+    def show_new_account_screen(self):
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'new_account_screen'
+
+
+class NewAccountScreen(MDScreen):
+
+    def new_account(self):
+        new_username = self.ids.new_user
+        new_password = self.ids.new_user_password
+        nombre = self.ids.nombre
+        apellido = self.ids.apellido
+
+        empty = validate_empty(new_username, new_password, nombre, apellido)
+
+        if empty:
+            self.ids.messages.text = "Todos los campos son requeridos."
+            return
+
+        res = database.get_user(new_username.text)
+
+        if (res['status'] == True):
+            self.ids.messages.text = "Ya existe una cuenta con este nombre de usuario."
+            return
+
+        database.create_user(
+            new_username.text, new_password.text, nombre.text, apellido.text)
+
+        self.ids.messages.theme_text_color = "Custom"
+        self.ids.messages.text_color = "green"
+        self.ids.messages.text = "Cuenta creada con exito!"
+
+        Clock.schedule_once(
+            lambda arg: (self.show_login_screen(), self.clean()), 2)
+
+    def show_login_screen(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+
+    def clean(self):
+        self.ids.messages.theme_text_color = "Error"
+        self.ids.messages.text = ""
+
+        self.ids.new_user.text = ""
+        self.ids.new_user_password.text = ""
+        self.ids.nombre.text = ""
+        self.ids.apellido.text = ""
+
+
+class AddBookDialog(MDBoxLayout):
+    menu_dropdown = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save)
+        date_dialog.open()
+
+    def on_save(self, instance, value, date_range):
+        date = value.strftime('%A %d %B %Y')
+        self.ids.fecha.text = str(date)
+
+    def menu_open_category(self):
+        categorias_libros = [
+            "Novela",
+            "Realismo mágico",
+            "Fantasía",
+            "Thriller",
+            "Novela gótica",
+            "Novela histórica",
+            "Novela filosófica",
+            "Ciencia ficción",
+            "Misterio",
+            "Aventura",
+            "Biografía",
+            "Poesía"
+        ]
+        menu_items = [
+            {
+                "text": categorias,
+                "on_release": lambda x=categorias: self.menu_callback(self.ids.categoria, x),
+            } for categorias in categorias_libros
+        ]
+        if not self.menu_dropdown:
+            self.menu_dropdown = MDDropdownMenu(
+                caller=self.ids.categoria, items=menu_items, position="bottom"
+            )
+
+            self.menu_dropdown.open()
+
+    def menu_open_lang(self):
+        menu_items = [
+            {
+                "text": "Español",
+                "on_release": lambda x=f"Español": self.menu_callback(self.ids.idioma, x),
+            },
+            {
+                "text": "Ingles",
+                "on_release": lambda x=f"Ingles": self.menu_callback(self.ids.idioma, x),
+            },
+            {
+                "text": "Alemán",
+                "on_release": lambda x=f"Aleman": self.menu_callback(self.ids.idioma, x),
+            },
+            {
+                "text": "Portugués",
+                "on_release": lambda x=f"Portugues": self.menu_callback(self.ids.idioma, x),
+            }
+        ]
+
+        if not self.menu_dropdown:
+            self.menu_dropdown = MDDropdownMenu(
+                caller=self.ids.idioma, items=menu_items, position="top"
+            )
+            self.menu_dropdown.open()
+
+    def menu_callback(self, text_box, text_item):
+        text_box.text = text_item
+        self.menu_dropdown.dismiss()
+        self.menu_dropdown = None
+
+
+class MiCard(MDCard):
+    titulo = StringProperty()
+    autor = StringProperty()
+    anyo = StringProperty()
+    precio = StringProperty()
+    categoria = StringProperty()
+    descripcion = StringProperty()
+    idioma = StringProperty()
+    imagen = StringProperty("images/default.jpg")
+
+    def __init__(self, libro_id=None, titulo="", imagen="", **kwargs):
+        super().__init__(**kwargs)
+        self.titulo = titulo
+        self.libro_id = libro_id
+
+        if os.path.exists(imagen):
+            self.imagen = imagen
+
+    def eliminar_libro(self, libro_id):
+        database.delete_book(libro_id)
+        self.parent.remove_widget(self)
+
+
+class AppScreen(MDScreen):
+    _add_book_dialog = None
+
+    def show_add_book_dialog(self):
+        if not self._add_book_dialog:
+            self._add_book_dialog = MDDialog(
+                title="Agregar libro",
+                type="custom",
+                content_cls=AddBookDialog(),
+            )
+        self._add_book_dialog.open()
+
+    def close_add_book_dialog(self):
+        self._add_book_dialog.dismiss()
+        self._add_book_dialog = None
+
+    def log_out(self):
+        # database.log_out_user()
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+    def on_pre_enter(self):
+        books = database.get_all_books()
+        grid = self.ids.grid
+
+        for book in books:
+            imagen = "images/" + str(book[0]) + ".jpg"
+            titulo = book[1]
+            new_widget = MiCard(libro_id=book[0], titulo=titulo, imagen=imagen)
+            grid.add_widget(new_widget)
+
+
+class MainApp(MDApp):
+    dialog = None
+
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Purple"
+        self.title = "Trabajo Final"
+
+        if platform != 'android':
+            Window.size = (900, 800)
+
+        self.manager = MDScreenManager()
+        self.manager.add_widget(LoginScreen(name='login_screen'))
+        self.manager.add_widget(AppScreen(name='app_screen'))
+        self.manager.add_widget(NewAccountScreen(name='new_account_screen'))
+
+        self.manager.current = "app_screen"
+
+        return self.manager
+
+    def show_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Usuario no registrado",
+                text="¿Desea crear una una nueva cuenta de usuario?",
+                buttons=[
+                    MDFlatButton(
+                        text="ACEPTAR",
+                        md_bg_color="purple",
+                        theme_text_color="Custom",
+                        text_color="white",
+                        on_release=lambda *args: self.show_new_account_screen(),
+                    ),
+                    MDFlatButton(
+                        text="CANCELAR",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=lambda *args: self.dialog.dismiss()
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def show_new_account_screen(self):
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'new_account_screen'
+        self.dialog.dismiss()
+
+    def close_add_book_dialog(self):
+        app_screen = self.root.get_screen('app_screen')
+        app_screen.close_add_book_dialog()
+
+    def add_book(self, titulo, categoria, autor, precio, descripcion, idioma, imagen, fecha):
+        print(titulo.text, categoria.text, autor.text, precio.text,
+              descripcion.text, idioma.text, imagen.text, fecha.text)
+
+        database.insert_book(titulo.text, autor.text, fecha.text, precio.text,
+                             categoria.text, descripcion.text, idioma.text)
+
+        app_screen = self.root.get_screen('app_screen')
+        grid = app_screen.ids.grid
+        new_widget = MiCard(titulo.text, imagen.text)
+        grid.add_widget(new_widget)
+
+
+MainApp().run()
